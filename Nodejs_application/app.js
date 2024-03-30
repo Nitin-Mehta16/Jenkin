26df49dbd4675848a993f24664da0fcaa8445de7
Nitin Mehta <115355852+nitin16503@users.noreply.github.com>
2024-03-30 12:03:24 +0530

    Update app.js

--- Nodejs_application/app.js
+++ Nodejs_application/app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   // Sending the response
-  res.end('Hemlo Dosto!\n');
+  res.end('Hemlo Friends!\n');
 });
 
 // Starting the server
